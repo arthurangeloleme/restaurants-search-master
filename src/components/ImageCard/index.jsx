35bde921b164369ed320b668dedb9364863b83cb
Arthur Angelo Leme <arthur.angelo@outlook.com>
2021-09-22 12:08:22 -0400

    Update Index ImageCard
    Atualizado arquivo com modificações da aula

--- src/components/ImageCard/index.jsx
+++ src/components/ImageCard/index.jsx
@@ -1,39 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
-import Text from '../Text';
 import ImageSkeleton from '../ImageSkeleton';
 
-export const Card = styled.div`
-  min-width: 90px;
+const Card = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 5px;
+  width: 90px;
   height: 90px;
-  border-radius: 8px;
+  border-radius: 6px;
   background-image: url(${(props) => props.photo});
   background-size: cover;
-  p {
-    margin-left: 6px;
-    margin-top: 10px;
-  }
 `;
 
-export default ({ restaurant }) => {
+const Title = styled.span`
+  font-family: ${(props) => props.theme.fonts.regular};
+  color: #fff;
+  font-size: 16px;
+`
+const ImageCard = ({ photo, title}) => {
   const [imageLoaded, setImageLoaded] = useState(false);
-
-  const image = restaurant.photos ? restaurant.photos[0].getUrl() : restaurant.icon;
 
   useEffect(() => {
     const imageLoader = new Image();
-    imageLoader.src = image;
+    imageLoader.src = photo;
     imageLoader.onload = () => setImageLoaded(true);
-  }, [image]);
+  }, [photo]);
 
   return (
     <>
       {imageLoaded ? (
-        <Card photo={image}>
-          <Text size="medium" color="#ffffff">
-            {restaurant.name}
-          </Text>
+        <Card photo={photo}>
+          <Title>{title}</Title>
         </Card>
       ) : (
         <ImageSkeleton width="90px" height="90px" />
@@ -41,3 +40,5 @@ export default ({ restaurant }) => {
     </>
   );
 };
+
+export default ImageCard;
